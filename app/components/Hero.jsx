1bad2866fda5d4775dfda655bef58e0f78f2107b
tanmayrane18 <tanmayrane51@gmail.com>
2023-12-18 07:48:00 +0530

    Pricing updated and hero animation removed

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
           <div className="bg-white p-3 translate-y-16  mb-8  w-[100%]   bg-no-repeat  bg-[url('/baner-right.png'),_url('/baner-left.png')] bg-[position:right_top,_left_top]">
             <div className=" px-10 w-full min-h-[calc(100vh-4rem)] overflow-hidden  flex flex-row flex-wrap max-md:flex-col max-md:justify-center max-md:items-center max-md:gap-[40px] justify-between items-center ">
               {/* left div */}
-              <div className="flex animate-moveLeft flex-col justify-start md:max-w-[48%]    items-start">
+              <div className="flex lg:animate-moveLeft flex-col justify-start md:max-w-[48%]    items-start">
                 <h6 className=" font-[500] text-[18px] mb-[15px] tracking-[2px] [word-spacing:2px]  uppercase">
                   welcome to{" "}
                   <span className="text-[#03a4ed] text-[30px] font-[700]">
@@ -25,7 +25,7 @@ const Hero = () => {
                 
               </div>
               {/* right div */}
-              <div className="flex flex-col relative animate-moveRight group justify-center items-center max-md:mb-5  w-full h-[50%] md:w-[42%] md:h-[50%]  ">
+              <div className="flex flex-col relative lg:animate-moveRight group justify-center items-center max-md:mb-5  w-full h-[50%] md:w-[42%] md:h-[50%]  ">
                 {/* <img
                 alt="side-image"
                   className="h-full w-full object-contain "
